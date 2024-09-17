cc9b5aa04b89e7dd58f3880f552b69044623c23f
setvect <setvect@naver.com>
2024-09-17 12:30:47 +0900

    텍스트 수정

--- src/components/HelpTab.tsx
+++ src/components/HelpTab.tsx
@@ -4,9 +4,9 @@ import { Box, Link, Typography, Avatar } from "@mui/material";
 const HelpTab: React.FC = () => {
   return (
     <Box sx={{ height: "100%", overflow: "auto", p: 2, display: "flex", flexDirection: "column", alignItems: "center" }}>
-      <Avatar src="/boksl.png" alt="복슬 지하철 로고" sx={{ width: 100, height: 100, mb: 2 }} />
+      <Avatar src="/boksl.png" alt="복슬지하철 로고" sx={{ width: 100, height: 100, mb: 2 }} />
       <Typography variant="h5" sx={{ mb: 1 }}>
-        복슬 지하철
+        복슬지하철
       </Typography>
       <Typography variant="body1" sx={{ mb: 2 }}>
         수도권 지하철 실시간 도착 안내
